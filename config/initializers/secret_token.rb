4a7548048a13c1fd0faef534fe1044412fa4b6a2
nilsenb <nilsenb@nilsenb-VirtualBox.(none)>
2011-12-30 01:14:07 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5c1d5f134a7c18cd71ddd9fac468499306cb253e7fd28a02e299e2f2fb24bf11531d8c836d0bf9babe88577e5f264d9f4f02ed5150f339e049770a3de2797061'
